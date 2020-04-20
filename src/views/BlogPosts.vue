ac3e39fa50f6724af9d6b34b2f31a283920bb15e
Ben Garcia <pokeboygarcia@gmail.com>
2020-04-19 20:24:38 -0700

    Added blank blog posts page.

---
+++ src/views/BlogPosts.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h2>Here are your posts!</h2>
+    </div>
+</template>
+
+<script lang="ts">
+import Component from "vue-class-component";
+import Vue from "vue";
+
+@Component({
+
+})
+export default class BlogPosts extends Vue {
+
+}
+</script>
